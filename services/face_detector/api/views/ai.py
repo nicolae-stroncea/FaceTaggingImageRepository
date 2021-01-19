879be6f67aa69c5516750e6a9c97f6a26e58d4ce
Nicolae Stroncea <stroncea.nicolae@gmail.com>
2021-01-19 04:30:35 +0000

    temp commit

--- services/face_detector/api/views/ai.py
+++ services/face_detector/api/views/ai.py
@@ -4,8 +4,10 @@ from flask import Blueprint, request, jsonify
 from webargs import fields
 from webargs.flaskparser import use_args
 from api.utils.core import logger
+# from api.utils.FaceDetector import 
 ai = Blueprint("ai", __name__, url_prefix="/")  # initialize blueprint
-
+scanning = False
+# from facenet_pytorch import MTCNN
 
 
 
@@ -15,3 +17,10 @@ def get():
     logger.info("getting resources")
     return jsonify({"test":"hello world"}), 200
 
+@ai.route("/start_scan", methods=['GET'])
+def start_scan():
+    global scanning
+    if not scanning:
+        scanning = True
+        logger.info("initializing scanning")
+    return jsonify({"test":"hello world"}), 200
